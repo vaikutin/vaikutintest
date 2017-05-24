3fb715636a9135cb79205c39e5312320fe81d616
Vaikutin <vaikutinfinland@gmail.com>
2017-05-24 12:47:35 +0300

    js externalized

---
+++ signup/initializeVaikutinFirebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBugLTZpuTgzwjv5xzob848SLFlOEqiZIs",
+    authDomain: "vaikutin-mvp.firebaseapp.com",
+    databaseURL: "https://vaikutin-mvp.firebaseio.com",
+    projectId: "vaikutin-mvp",
+    storageBucket: "vaikutin-mvp.appspot.com",
+    messagingSenderId: "1029926726580"
+  };
+  firebase.initializeApp(config);
